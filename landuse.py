4ecb42c0827a40a26bcc5d14ebd6dae5403563ec
Milo van der Linden <milo@dogodigi.net>
2011-05-26 19:34:25 +0200

    more landuse

--- landuse.py
+++ landuse.py
@@ -10,6 +10,15 @@ fts = sld.Fts()
 f = sld.OgcFilter('landuse','=','forest')
 fts.addRule(sld.Rule('forest', 0,120000,f, sld.Polygon('#6b942e','#6b942e',1)))
 
+#residential
+f = sld.OgcFilter('landuse','=','residential')
+fts.addRule(sld.Rule('residential', 0,120000,f, sld.Polygon('#e6e6e6','#e6e6e6',1)))
+
+#residential
+f = sld.OgcFilter('landuse','=','grass') #Or landuse=village_green
+fts.addRule(sld.Rule('grass', 0,120000,f, sld.Polygon('#aac566','#aac566',1)))
+
+#aac566
 s.addFts(fts)
 
 # export
